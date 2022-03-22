7460693900c644f5f500d68d3dfce9a55b01f421
NanPark <nemyrmain@gmail.com>
2022-03-23 04:40:14 +0900

    search page

--- lib/search_page.dart
+++ lib/search_page.dart
@@ -1,28 +1,100 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'main.dart';
+import 'package:ask_it/main.dart';
 
 class SearchPage extends StatefulWidget {
-  SearchPage({Key? key}) : super(key: key);
-
   @override
   State<SearchPage> createState() => _SearchPageState();
 }
 
 class _SearchPageState extends State<SearchPage> {
+  //변수
+  final searchInputController = TextEditingController();
+  FocusNode textSearchFocus = FocusNode();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SafeArea(
-        child: Column(
-          children: [
-            ElevatedButton(
-                onPressed: (){},
-                child: Text("To view question")),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: Text("Pop"))
-          ],
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 29, 30, 37),
+        body: SafeArea(
+          //앱 전체 감싸는 div
+          child: Container(
+            child: Column(
+              children: [
+                //상단바 div
+                Container(
+                  height: 70,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        iconSize: 35,
+                        icon: const Icon(
+                          Icons.close,
+                          color: Colors.white,
+                        ),
+                      ),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.7,
+                        child: TextFormField(
+                          controller: searchInputController,
+                          focusNode: textSearchFocus,
+                          style: const TextStyle(
+                              fontSize: 20, color: Colors.white),
+                          decoration: InputDecoration(
+                            isDense: true,
+                            contentPadding:
+                                const EdgeInsets.fromLTRB(14, 8, 8, 8),
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            fillColor: Colors.white,
+                            filled: true,
+                            hintText: "Search",
+                            hintStyle: const TextStyle(
+                              color: Color.fromARGB(255, 125, 125, 125),
+                              fontFamily: 'Montserrat',
+                              fontSize: 20,
+                            ),
+                          ),
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        iconSize: 35,
+                        icon: const Icon(
+                          Icons.search,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const Expanded(
+                  child: Center(
+                    child: Text(
+                      "Search Anything!",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'Montserrat',
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
